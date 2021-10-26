c469057afbb8f7380dd6e7c9ca2e8a628b7a83ef
Rory <rory.dwy@gmail.com>
2021-10-26 09:16:29 -0700

    Change component to Ref

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,37 +1,36 @@
-import { useState, useEffect } from "react";
-import Node from "./Node";
+import { useState, useEffect, createRef, useRef } from "react";
+// import Node from "./Node";
 import { getUnvisitedNeighbors } from "../algorithms/dijkstra";
 
 const Grid = () => {
-  const [grid, setGrid] = useState([]);
+  //   const [grid, setGrid] = useState([]);
   const [mouseIsPressed, setMouseIsPressed] = useState(false);
 
   const cols = 40;
   const rows = 25;
+  const width = 1024 / cols + "px";
   let start = 210; // ID of start
   let end = 240; // ID of end
 
+  const grid = Array(cols * rows)
+    .fill({ id: 0, visited: false, isWall: false })
+    .map((node, i) => ({ ...node, id: i }));
+
+  const nodeRefs = useRef([]);
+  nodeRefs.current = grid.map((el, i) => nodeRefs.current[i] ?? createRef());
+
   // LifeCycle Hook
-  useEffect(() => {
-    // Create node list with id's acending
-    setGrid(
-      Array(cols * rows)
-        .fill({ id: 0, visited: false, isWall: false })
-        .map((node, i) => ({ ...node, id: i }))
-    );
-  }, []);
+  useEffect(() => {}, []);
 
   // Handle Wall Key press
-  const handleMouseDown = (node) => {
-    grid[node].isWall = true;
-    setGrid([...grid]);
+  const handleMouseDown = (id) => {
+    nodeRefs.current[id].current.classList.toggle("node-wall");
     setMouseIsPressed(true);
   };
 
-  const handleMouseEnter = (node) => {
+  const handleMouseEnter = (id) => {
     if (!mouseIsPressed) return;
-    grid[node].isWall = true;
-    setGrid([...grid]);
+    nodeRefs.current[id].current.classList.toggle("node-wall");
   };
 
   const handleMouseUp = () => {
@@ -43,25 +42,25 @@ const Grid = () => {
       <button
         className="border-2 border-blue-400 rounded p-3 m-3"
         onClick={() => {
-          //   setGrid([...getUnvisitedNeighbors(grid, grid[start], cols)]);
-          getUnvisitedNeighbors(grid, grid[start], cols, setGrid);
-          //   console.log(grid);
+          //   getUnvisitedNeighbors(grid, grid[start], cols, setGrid);
+          console.log(this.refs.node4);
         }}
       >
         Start
       </button>
       <div id="grid" className="flex flex-wrap max-w-screen-lg">
         {grid.map((n) => (
-          <Node
+          <div
             key={n.id}
-            node={n}
-            cols={cols}
-            start={start}
-            end={end}
-            onMouseDown={handleMouseDown}
-            onMouseEnter={handleMouseEnter}
-            onMouseUp={handleMouseUp}
-          />
+            ref={nodeRefs.current[n.id]}
+            className={`border border-black ${
+              start === n.id ? "node-start" : end === n.id ? "node-end" : n.isWall ? "node-wall" : n.visited === true ? "node-visited" : ""
+            }`}
+            style={{ width: width, height: width }}
+            onMouseDown={() => handleMouseDown(n.id)}
+            onMouseEnter={() => handleMouseEnter(n.id)}
+            onMouseUp={() => handleMouseUp()}
+          ></div>
         ))}
       </div>
     </div>
